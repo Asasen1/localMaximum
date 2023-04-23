e5d38e51c5c2fbf8c1868be31ec2d3117ee619d3
Asasen1 <artemiy1astanin@gmail.com>
2023-04-23 22:07:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[30];
+        Random rand = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(0, 101);
+        }
+        Console.WriteLine("Массив:");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.Write("Локальные максимумы:");
+        if (array[0] >= array[1])
+            Console.Write(array[0] + " ");
+        for (int i = 1; i < array.Length - 1; i++)
+        {
+            if (array[i] > array[i - 1] && array[i] > array[i + 1])
+                Console.Write(array[i] + " ");
+        }
+        if (array.Length - 1 >= array.Length - 2)
+        { // проверка последнего элемента
+            Console.Write(array[array.Length - 1] + " ");
+        }
+    }
+}
